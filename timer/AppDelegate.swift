8a596a5d35917480de8c4b59f0750871201ce93d
Sergio Baró <serbarsim@gmail.com>
2020-04-05 07:30:10 +0200

    Initial Commit

---
+++ timer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  timer
+//
+//  Created by Sergio Baro on 05/04/2020.
+//  Copyright © 2020 Sergio Baro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
