aab791be061a68c1243298edbb838bed21f3d3ae
Sergio Baró <serbarsim@gmail.com>
2020-04-12 07:43:00 +0200

    Some renaming

--- Common/Localization/Localization.swift
+++ Common/Localization/Localization.swift
@@ -1,12 +1,5 @@
 import Foundation
 
-extension Bundle {
-  static func `for`(_ object: AnyObject?) -> Bundle? {
-    guard let object = object else { return nil }
-    return Bundle(for: type(of: object))
-  }
-}
-
 public func loc(_ key: String, _ object: AnyObject?) -> String {
   let bundle = Bundle.for(object) ?? .main
   return loc(key, bundle)
@@ -14,4 +7,11 @@ public func loc(_ key: String, _ object: AnyObject?) -> String {
 
 public func loc(_ key: String, _ bundle: Bundle) -> String {
   return NSLocalizedString(key, tableName: nil, bundle: bundle, value: "", comment: "")
+}
+
+private extension Bundle {
+  static func `for`(_ object: AnyObject?) -> Bundle? {
+    guard let object = object else { return nil }
+    return Bundle(for: type(of: object))
+  }
 }
