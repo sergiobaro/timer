2c1602c51810dbd2fe394edd353c33ecac811746
Sergio Baró <serbarsim@gmail.com>
2020-04-17 14:04:03 +0200

    Added stepper to time selector

--- TimeSelector/TimeSelector/TimeSelectorViewController.swift
+++ TimeSelector/TimeSelector/TimeSelectorViewController.swift
@@ -6,7 +6,9 @@ class TimeSelectorViewController: NSViewController {
   weak var delegate: TimeSelectorDelegate?
 
   @IBOutlet private weak var nameTextField: NSTextField!
+  @IBOutlet private weak var minutesLabel: NSTextField!
   @IBOutlet private weak var minutesTextField: NSTextField!
+  @IBOutlet private weak var minutesStepper: NSStepper!
   @IBOutlet private weak var startButton: NSButton!
 
   override func viewDidAppear() {
@@ -15,16 +17,25 @@ class TimeSelectorViewController: NSViewController {
     view.window?.title = loc("time.selector.title", self)
 
     nameTextField.placeholderString = loc("time.selector.name.placeholder", self)
+    minutesLabel.stringValue = loc("time.selector.minutes.label", self)
     minutesTextField.placeholderString = loc("time.selector.minutes.placeholder", self)
     minutesTextField.formatter = MinutesFormatter()
     startButton.title = loc("time.selector.start.button", self)
     startButton.keyEquivalent = "\r"
 
+    minutesStepper.maxValue = 9999
+    minutesStepper.target = self
+    minutesStepper.action = #selector(minutesStepperDidChange)
+
+    minutesTextField.delegate = self
+
     nameTextField.becomeFirstResponder()
   }
 
   @IBAction private func tapStartButton(_ sender: NSButton) {
-    guard let minutes = Int(minutesTextField.stringValue) else {
+    guard let minutes = Int(minutesTextField.stringValue),
+      minutes > 0
+    else {
       return
     }
 
@@ -32,6 +43,19 @@ class TimeSelectorViewController: NSViewController {
 
     let timeInterval = TimeInterval(minutes * 60)
     delegate?.timeSelectorDidSelect(name: nameTextField.stringValue, timeInterval: timeInterval)
+  }
+
+  @objc private func minutesStepperDidChange(_ sender: NSStepper) {
+    let value = sender.integerValue
+    minutesTextField.stringValue = String(value)
+  }
+}
+
+extension TimeSelectorViewController: NSTextFieldDelegate {
+
+  func controlTextDidChange(_ obj: Notification) {
+    guard let integerValue = Int(minutesTextField.stringValue) else { return }
+    minutesStepper.integerValue = integerValue
   }
 }
 
